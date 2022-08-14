07aabc9e7bb3c888637d7803b3f0b980eafade29
Maxim-Pekov <64793556+Maxim-Pekov@users.noreply.github.com>
2022-08-15 02:40:12 +0300

    add ifname function

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import pathlib
 from urllib.request import urlretrieve
 
 
-def dunload_images(url, directory_path):
+url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+
+
+def download_images(url, directory_path):
     name_photo = url.split("/")[-1]
     pathlib.Path(directory_path).mkdir(parents=True, exist_ok=True)
     urlretrieve(url, f'{directory_path}/{name_photo}')
 
 
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-
-dunload_images(url, directory_path='images')
-
+if __name__ == "__main__":
+    download_images(url, directory_path='images')
