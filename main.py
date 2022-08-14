8193bca1b3d542f9d5122489647bda7b84051f38
Maxim-Pekov <64793556+Maxim-Pekov@users.noreply.github.com>
2022-08-15 02:35:44 +0300

    add download photo function

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import pathlib
 from urllib.request import urlretrieve
 
 
+def dunload_images(url, directory_path):
+    name_photo = url.split("/")[-1]
+    pathlib.Path(directory_path).mkdir(parents=True, exist_ok=True)
+    urlretrieve(url, f'{directory_path}/{name_photo}')
+
+
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 
-pathlib.Path('images').mkdir(parents=True, exist_ok=True)
+dunload_images(url, directory_path='images')
 
-urlretrieve(url, 'images/hubble.jpeg')
