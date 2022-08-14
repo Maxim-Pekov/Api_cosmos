709b4ba15cdc472507dfa9ecda20b7ce54e03957
Maxim-Pekov <64793556+Maxim-Pekov@users.noreply.github.com>
2022-08-15 01:31:35 +0300

    add code to auto created images directory

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import pathlib
+
 from urllib.request import urlretrieve
+
 
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 
-urlretrieve(url, 'hubble.jpeg')
+pathlib.Path('images').mkdir(parents=True, exist_ok=True)
+
+urlretrieve(url, 'images/hubble.jpeg')
