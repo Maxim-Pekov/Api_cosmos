bf147133e135be08ce49b4e4362e976a7d3a4d18
Maxim-Pekov <64793556+Maxim-Pekov@users.noreply.github.com>
2022-08-15 23:19:24 +0300

    add a function for downloading pictures from the space_x api

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import pathlib
+import pathlib, requests
 
+from pprint import pprint
 from urllib.request import urlretrieve
 
 
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+directory_path='images'
 
 
 def download_images(url, directory_path):
@@ -12,5 +14,13 @@ def download_images(url, directory_path):
     urlretrieve(url, f'{directory_path}/{name_photo}')
 
 
+def main():
+    response = requests.get('https://api.spacexdata.com/v5/launches/61e048ffbe8d8b66799018d1')
+    space_urls = response.json()['links']['flickr']['original']
+    for url in space_urls:
+        print(url)
+        download_images(url, directory_path)
+
+
 if __name__ == "__main__":
-    download_images(url, directory_path='images')
+    main()
