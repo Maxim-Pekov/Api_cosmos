830c8ed4554fb89bf332ad5766fdeb494a5afcaf
Maxim-Pekov <64793556+Maxim-Pekov@users.noreply.github.com>
2022-08-17 10:43:29 +0300

    add function to get nasa photos

--- main.py
+++ main.py
@@ -1,40 +1,65 @@
+import os.path
 import pathlib
+from os.path import splitext
 from pprint import pprint
+from urllib.parse import urlparse
 
 import requests
 from urllib.request import urlretrieve
 from environs import Env
 
+
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 directory_path = 'images'
+env = Env()
+env.read_env()
 
 
-def download_images(url, directory_path, name_photo=None):
-    if name_photo is None:
-        name_photo = url.split("/")[-1]
-    pathlib.Path(directory_path).mkdir(parents=True, exist_ok=True)
-    urlretrieve(url, f'{directory_path}/{name_photo}.jpeg')
+def save_images(photos, directory_path, name_photo=None):
+    """Создает директорию {directory_path} в корне проекта и сохраняет туда фотографии из переданных списком ссылок"""
+    for number_url, url in enumerate(photos):
+        extension = get_extension(url)
+        pathlib.Path(directory_path).mkdir(parents=True, exist_ok=True)
+        urlretrieve(url, f'{directory_path}/{name_photo}{number_url}{extension}')
 
 
-def fetch_spacex_launch_by_id(flight_id):
+def get_spacex_photo_launch_by_id(flight_id):
     response = requests.get(f'https://api.spacexdata.com/v5/launches/{flight_id}')
+    response.raise_for_status()
     spacex_photo_urls = response.json()['links']['flickr']['original']
     return spacex_photo_urls
 
 
+def get_extension(url):
+    parse_result = urlparse(url)
+    url_path = parse_result.path
+    extension = splitext(url_path)[1]
+    return extension
+
+
+def get_nasa_photos_urls(number_of_urls):
+    url = 'https://api.nasa.gov/planetary/apod'
+    params = {
+        'api_key': env.str('NASA_TOKEN'),
+        'count': number_of_urls
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    info_photos = response.json()
+    nasa_photos_urls = []
+    for info_photo in info_photos:
+        nasa_photos_urls.append(info_photo['hdurl'])
+    return nasa_photos_urls
+
+
 def main():
-    spacex_photo_urls = fetch_spacex_launch_by_id('61e048ffbe8d8b66799018d1')
-    for number_url, url in enumerate(spacex_photo_urls):
-        download_images(url, directory_path, f'spacex_{number_url}')
+    nasa_photo_urls = get_nasa_photos_urls(5)
+    spacex_photo_urls = get_spacex_photo_launch_by_id('61e048ffbe8d8b66799018d1')
+    save_images(nasa_photo_urls, directory_path, 'nasa')
+    save_images(spacex_photo_urls, directory_path, 'spacex')
 
 
 if __name__ == "__main__":
     main()
-    env = Env()
-    env.read_env()
-    urll = 'https://api.nasa.gov/planetary/apod'
-    params = {
-        'api_key': env.str('NASA_TOKEN'),
-    }
-    resp = requests.get(urll, params=params)
-    pprint(resp.json()['hdurl'])
+
+
