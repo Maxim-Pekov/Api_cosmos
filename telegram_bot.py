6d4fc50da83c5f5406380c30337fea0334c806c3
Maxim-Pekov <64793556+Maxim-Pekov@users.noreply.github.com>
2022-08-20 00:45:41 +0300

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,21 +11,31 @@ env.read_env()
 token = env.str('TOKEN')
 
 
-def message_handler(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        text='Hi Max',
-    )
+# def message_handler(update: Update, context: CallbackContext):
+#     update.message.reply_text(
+#         text='Hi Max',
+#     )
+#
+# def main():
+#     print('Start')
+#     updater = Updater(
+#         token=token,
+#         use_context=True
+#     )
+#     updater.dispatcher.add_handler(MessageHandler(filters=Filters.all, callback=message_handler))
+#
+#     updater.start_polling()
+#     updater.idle()
+#
+# if __name__ == '__main__':
+#     main()
+bot = telegram.Bot(token=token)
+print(bot.get_me())
+# updates = bot.get_updates()
+# print(updates[0])
 
-def main():
-    print('Start')
-    updater = Updater(
-        token=token,
-        use_context=True
-    )
-    updater.dispatcher.add_handler(MessageHandler(filters=Filters.all, callback=message_handler))
+bot.send_message(text='Hi John!', chat_id=-1001626507075)
+bot.send_document(chat_id=-1001626507075, document=open('images/nasa_0.jpg', 'rb'))
 
-    updater.start_polling()
-    updater.idle()
 
-if __name__ == '__main__':
-    main()
+# bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
